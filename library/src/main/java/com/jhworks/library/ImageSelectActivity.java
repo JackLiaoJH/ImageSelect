efd55de071b9be4f2d144e6f70a6f12820247cfd
ljh105520 <ljh105520@alibaba-inc.com>
2016-07-27 13:10:48 +0800

    Initial commit

---
+++ library/src/main/java/com/jhworks/library/ImageSelectActivity.java
@@ -0,0 +1,18 @@
+package com.jhworks.library;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Author : LiaoJH
+ * Desc :
+ * Date: 2016/7/27
+ */
+public class ImageSelectActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
