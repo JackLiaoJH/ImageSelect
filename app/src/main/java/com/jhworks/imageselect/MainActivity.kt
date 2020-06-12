1391e63cd845ce6cd4d2f9d4c1fb0901e31e8218
jackson <jackson>
2020-06-12 13:50:17 +0800

    修改脚本

--- app/src/main/java/com/jhworks/imageselect/MainActivity.kt
+++ app/src/main/java/com/jhworks/imageselect/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -133,6 +134,7 @@ class MainActivity : AppCompatActivity() {
                     sb.append("\n")
                 }
                 mResultText?.text = sb.toString()
+                Log.e("ImageSelect", "结果： $mSelectPath")
             }
         }
     }
